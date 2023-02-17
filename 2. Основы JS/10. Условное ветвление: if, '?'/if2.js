384a6d5dae80a0f90e224af5b06c87e5a9a1d9f8
Sevindzh Chebanenko <misschebanenko@gmail.com>
2023-02-17 19:11:26 +0300

    basics js

---
+++ 2. Основы JS/10. Условное ветвление: if, '?'/if2.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let nameJS = prompt('Какое "официальное" название JavaScript?', "");
+
+if (nameJS == "ECMAScript") {
+  alert("Верно!");
+} else {
+  alert("Не знаете? ECMAScript!");
+}
